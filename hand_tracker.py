26a854a935e25c3f88ecfcec6976ce95d0a02531
rabeya100X <rabeyanoor.dev@gmail.com>
2026-09-02 18:12:37 +0600

    Fix MediaPipe solutions AttributeError in hand_tracker

--- hand_tracker.py
+++ hand_tracker.py
@@ -5,21 +5,35 @@ Provides hand detection, 21 landmark extraction, and spatial 3D coordinate utili
 
 import cv2
 import mediapipe as mp
+try:
+    import mediapipe.python.solutions.hands as mp_hands
+    import mediapipe.python.solutions.drawing_utils as mp_draw
+    import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
+except ImportError:
+    mp_hands = mp.solutions.hands
+    mp_draw = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
 import math
 import numpy as np
 
 
 class HandTracker:
     def __init__(self, max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.7):
-        self.mp_hands = mp.solutions.hands
+        try:
+            self.mp_hands = mp.solutions.hands
+            self.mp_draw = mp.solutions.drawing_utils
+            self.mp_drawing_styles = mp.solutions.drawing_styles
+        except AttributeError:
+            self.mp_hands = mp_hands
+            self.mp_draw = mp_draw
+            self.mp_drawing_styles = mp_drawing_styles
+
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
             max_num_hands=max_num_hands,
             min_detection_confidence=min_detection_confidence,
             min_tracking_confidence=min_tracking_confidence
         )
-        self.mp_draw = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
 
         # Fingertip and Joint IDs
         self.FINGERTIP_IDS = [4, 8, 12, 16, 20]  # Thumb, Index, Middle, Ring, Pinky
